1e2ad536751937d911542d382a6e8a288145236b
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-23 16:09:52 +0000

    Add PostWidget and Categories components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,8 @@ export default function Home() {
           </div>
           <div className="lg:col-span-4 col-span-1">
             <div className="lg:sticky relative top-8">
-
+              <PostWidget />
+              <Categories />
             </div>
           </div>
         </div>
