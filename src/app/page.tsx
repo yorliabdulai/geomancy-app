7e9b878942f08be40914837519a3e84f258a92fd
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-23 15:46:29 +0000

    Refactor grid layout in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
           <link rel="icon" href="/favicon.ico" />
         </Head>
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-          <div >
+          <div className="lg:col-span-8 col-span-1">
            {posts.map((post, index) => (
            <div>
               {post.title}
