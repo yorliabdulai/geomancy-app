1f594a0b34f3cb712bb6a25f0532a99b69de4293
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-24 11:46:40 +0000

    Refactor navigation component to use a separate Sidebar component

--- src/components/navigation/index.tsx
+++ src/components/navigation/index.tsx
@@ -1,20 +1,56 @@
-"use client";
-import { useState } from "react";
-import Navbar from "./navbar";
-import Sidebar from "./sidebar";
+import Link from "next/link";
 
-const Navigation = () => {
-  // toggle sidebar
-  const [isOpen, setIsOpen] = useState(false);
-  const toggle = () => {
-    setIsOpen(!isOpen);
-  };
+const Sidebar = ({
+  isOpen,
+  toggle,
+}: {
+  isOpen: boolean;
+  toggle: () => void;
+}): JSX.Element => {
   return (
     <>
-      <Sidebar isOpen={isOpen} toggle={toggle} />
-      <Navbar toggle={toggle} />
+      <div
+        className="sidebar-container fixed w-full h-full overflow-hidden justify-center bg-white grid pt-[120px] left-0 z-10"
+        style={{
+          opacity: `${isOpen ? "1" : "0"}`,
+          top: ` ${isOpen ? "0" : "-100%"}`,
+        }}
+      >
+        <button className="absolute right-0 p-5" onClick={toggle}>
+          {/* Close icon */}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="48"
+            height="48"
+            viewBox="0 0 24 24"
+          >
+            <path
+              fill="currentColor"
+              d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+            />
+          </svg>
+        </button>
+
+        <ul className="sidebar-nav text-center leading-relaxed text-xl">
+          <li>
+            <Link href="/about" onClick={toggle}>
+              <p>About Us</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/services" onClick={toggle}>
+              <p>Services</p>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contacts" onClick={toggle}>
+              <p>Contacts</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </>
   );
 };
 
-export default Navigation;
+export default Sidebar;
