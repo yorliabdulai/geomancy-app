06bddba13314957346c3c7c6b41445ee31548eac
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-24 11:55:29 +0000

    Refactor navbar component to include toggle button and update links

--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -3,36 +3,50 @@ import Link from "next/link";
 import Logo from "./Logo";
 import Button from "./Button";
 
-const Navbar = () => {
+const Navbar = ({ toggle }: { toggle: () => void }) => {
   return (
     <>
       <div className="w-full h-20 bg-emerald-800 sticky top-0">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             <Logo />
-            <ul className="hidden md:flex gap-x-6 text-white">
+            <button
+              type="button"
+              className="inline-flex items-center md:hidden"
+              onClick={toggle}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="40"
+                height="40"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#fff"
+                  d="M3 6h18v2H3V6m0 5h18v2H3v-2m0 5h18v2H3v-2Z"
+                />
+              </svg>
+            </button>
+            <ul className="hidden md:flex gap-x-6 text-white ">
               <li>
                 <Link href="/about">
                   <p>About Us</p>
                 </Link>
               </li>
               <li>
-                <Link href="/blog">
-                  <p>Blog</p>
+                <Link href="/services">
+                  <p>Services</p>
                 </Link>
               </li>
               <li>
-                <Link href="/learn">
-                  <p>Learn</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/contact">
-                  <p>Contact</p>
+                <Link href="/contacts">
+                  <p>Contacts</p>
                 </Link>
               </li>
             </ul>
-            <Button />
+            <div className="hidden md:block">
+              <Button />
+            </div>
           </div>
         </div>
       </div>
