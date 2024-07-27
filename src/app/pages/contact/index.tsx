a7a01a2c5faeb66c92391ca18408a688caf4809d
yorliabdulai <yorliabdulai1@gmail.com>
2024-07-27 16:23:05 +0000

    Update contact and learn pages

--- src/app/pages/contact/index.tsx
+++ src/app/pages/contact/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
-function Contact() {
-  return (
+const Contact = () => (
     <div>
-      
+      <h1>Contact Us</h1>
+      <p>Welcome to the contact page.</p>
     </div>
-  )
-}
-
-export default Contact
+  );
+  
+  export default Contact;
+  
--- src/app/pages/learn/index.tsx
+++ src/app/pages/learn/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-
-function Learn() {
-  return (
+const Contact = () => (
     <div>
-      
+      <h1>Learn</h1>
+      <p>Welcome to the learn page.</p>
     </div>
-  )
-}
-
-export default Learn
+  );
+  
+  export default Contact;
+  
