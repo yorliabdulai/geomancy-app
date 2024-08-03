622f5b9c9e79d8d0a337c894b5c3d98b9926e2a0
yorliabdulai <yorliabdulai1@gmail.com>
2024-08-03 11:09:14 +0000

    Update component imports

--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,10 @@ export {default as PostWidget} from '../components/PostWidget';
 export {default as Categories} from '../components/Categories';
 export {default as Header} from '../components/Header';
 
-export { default as PostDetail } from './PostDetail';
-export { default as Layout } from './Layout';
+export { default as PostDetail } from '../components/PostDetail';
+export { default as Layout } from '../components/Layout';
 
-export { default as Author } from './Author';
+export { default as Author } from '../components/Author';
 
 export { default as AdjacentPostCard } from '../components/AdjacentPostCard';
 export { default as FeaturedPostCard } from '../components/FeaturedPostCard';
