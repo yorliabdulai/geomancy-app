6fc3c2555200120d61d12f6bf8cd03502f5139aa
yorliabdulai <yorliabdulai1@gmail.com>
2024-08-03 08:14:10 +0000

    Add navigation component to Layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Header from '../components/Header';
+import Navigation from "../components/navigation/index";
 
 const Layout = ({ children }) => (
   <>
+    <Navigation />
     <Header />
     {children}
   </>
