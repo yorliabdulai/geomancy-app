720b387a9cc6b75306caa23e0cbde6b986a70baa
yorliabdulai <yorliabdulai1@gmail.com>
2024-08-04 09:27:34 +0000

    added another api endpoint

---
+++ pages/api/books/[id].js
@@ -0,0 +1,14 @@
+import { connectToDatabase } from '../../../utils/mongodb';
+import { ObjectId } from 'mongodb';
+
+export default async function handler(req, res) {
+  const { db } = await connectToDatabase();
+  const { id } = req.query;
+
+  if (req.method === 'DELETE') {
+    await db.collection('books').deleteOne({ _id: new ObjectId(id) });
+    res.status(204).end();
+  } else {
+    res.status(405).json({ message: 'Method not allowed' });
+  }
+}
