b15ba2cb751f8b6d3924030ad1e6e8ac01ce53d9
yorliabdulai <yorliabdulai1@gmail.com>
2024-08-05 17:11:27 +0000

    Update ApolloClient URI to use environment variable

--- src/lib/apolloClient.js
+++ src/lib/apolloClient.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://your-graphql-endpoint', // Replace with your GraphCMS endpoint
+  uri: process.env.BOOKS_API_ENDPOINT, // Replace with your GraphCMS endpoint
   cache: new InMemoryCache(),
   headers: {
     authorization: `Bearer your-api-token`, // Replace with your API token
