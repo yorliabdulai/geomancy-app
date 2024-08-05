c92886c6ab25bc25aad69bbb5b2f71d160c1e1c6
yorliabdulai <yorliabdulai1@gmail.com>
2024-08-05 15:40:46 +0000

    Refactor MongoDB connection and add PUT method to update order status

--- pages/api/orders/index.js
+++ pages/api/orders/index.js
@@ -1,12 +1,28 @@
-import { connectToDatabase } from '../../../utils/mongodb';
+import { MongoClient } from 'mongodb';
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 export default async function handler(req, res) {
-  const { db } = await connectToDatabase();
+  try {
+    await client.connect();
+    const database = client.db('your_database_name');
+    const orders = database.collection('orders');
 
-  if (req.method === 'GET') {
-    const orders = await db.collection('orders').find().toArray();
-    res.status(200).json(orders);
-  } else {
-    res.status(405).json({ message: 'Method not allowed' });
+    if (req.method === 'GET') {
+      const allOrders = await orders.find({}).toArray();
+      res.status(200).json(allOrders);
+    } else if (req.method === 'PUT') {
+      const { id, status } = req.body;
+      const updatedOrder = await orders.updateOne({ _id: id }, { $set: { status } });
+      res.status(200).json(updatedOrder);
+    } else {
+      res.setHeader('Allow', ['GET', 'PUT']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Unable to connect to the database' });
+  } finally {
+    await client.close();
   }
 }
